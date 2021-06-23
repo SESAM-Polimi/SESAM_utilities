72feda0336559902d35103878b8e9241c74727c2
LorenzoRinaldi <lorenzo.rinaldi1@gmail.com>
2021-06-23 17:00:25 +0200

    new functions

--- utilities.py
+++ utilities.py
@@ -5,6 +5,10 @@ Author: Lorenzo Rinaldi, SESAM
 
 import pandas as pd
 import bibtexparser
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+import os
+# from os.path import isfile, join
 
 
 #%%
@@ -23,7 +27,6 @@ class functions():
     
     def bibtex_parser(bib_path:str, xlsx_path:str, properties=['year', 'title', 'author', 'journal', 'doi'], doi_links=True):
         
-        
         """
         This function generates an .xlsx file from a list of documents contained in a .bib file
         
@@ -36,7 +39,6 @@ class functions():
         Returns:
             selection: pandas DataFrame containing the parsed information of about the imported documents contained in the .bib file
         """
-        
         
         with open(bib_path, encoding="utf8") as bibtex_file:
             bib_database = bibtexparser.load(bibtex_file)
@@ -71,3 +73,45 @@ class functions():
         selection.to_excel(xlsx_path, index=False)
         
         return selection
+        
+    
+    def get_files_path_from_directory(path, file_extension:str):
+        
+        filenames = os.listdir(path)
+        
+       
+        for file in filenames:
+            if file[-len(file_extension)] != file_extension:
+                filenames.remove(file)
+               
+        return filenames
+
+
+    def elsevier_pdf_metadata_miner(pdf_path:list):
+
+        """
+        This function generates a dictionary containing metadata information of one or multiple pdf articles downloaded from Elsevier
+        
+        Args:
+            pdf_path: list containing the paths of the files to import
+        
+        Returns:
+            info: dictionary containing metadata information of the imported pdf articles
+        """
+        
+        fp = open(pdf_path, 'rb')
+        parser = PDFParser(fp)
+        doc = PDFDocument(parser)
+        info = {}
+        for i in doc.info[0].keys():
+            try:
+                info[i] = doc.info[0][i].decode("utf-8")
+            except:
+                pass
+            
+        return info
+
+        
+                
+        
+        
